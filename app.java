28ef66a5e37f83cbe1d05ccfe6e4caa3c29e307a
Gowthamsaivarma <gowthamsaivarma000@gmail.com>
2026-09-20 19:42:39 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,9 +1,11 @@
-this is used to practice java and devops 
-first build completed now second build
-testing whether the jenkins is integrated with git or not
-  i am changing this so i want to know whether the job scheduling is working or not
-build periodically is working now want to test poll scm the scedule time is at 355
-  adding new line
-adding new line at 57
-  new build at 58
-now poll scm should stop working
+yum install java-21-amazon-corretto -y
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.118/bin/apache-tomcat-9.0.118.tar.gz
+tar -zxvf apache-tomcat-9.0.118.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.118/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.118/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.118/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.118/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.118/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-9.0.118/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-9.0.118/webapps/manager/META-INF/context.xml
+sh apache-tomcat-9.0.118/bin/startup.sh
